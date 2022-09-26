df723e0826ef889971981f7b48aa09e1feea3502
Rom2-ls <capolunghiromain@gmail.com>
2022-09-26 17:16:04 +0200

    ADD conn to database, ADD product to database

--- app/src/Controller/LuckyController.php
+++ app/src/Controller/LuckyController.php
@@ -6,9 +6,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route('/lucky')]
 class LuckyController extends AbstractController
 {
-    #[Route('/lucky/number/{max}', name: 'app_lucky_number')]
+    #[Route('/number/{max}')]
     public function number(int $max, LoggerInterface $logger): Response
     {
         $logger->info('You are betch');
---
+++ app/src/Controller/ProductController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'create_product')]
+    public function createProduct(ManagerRegistry $doctrine, ValidatorInterface $validator): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setPrice(1999);
+        $product->setDescription('Ergonomic and stylish!');
+
+        $errors = $validator->validate($product);
+        if (count($errors) > 0) {
+            return new Response((string) $errors, 400);
+        }
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
+    }
+}
